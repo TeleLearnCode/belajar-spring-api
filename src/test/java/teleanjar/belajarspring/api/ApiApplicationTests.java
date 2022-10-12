13358400e6d49bebc37a3a32e6370e15c8cec852
sahtia murti <sahtiamurti@gmail.com>
2022-10-12 16:13:51 +0700

    first init

---
+++ src/test/java/teleanjar/belajarspring/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package teleanjar.belajarspring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
